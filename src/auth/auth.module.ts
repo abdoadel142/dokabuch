1d74fdd4f634abae1d21a2d6c8d6b2af6a54fd79
Abdelrhman Adel <abdoadel142@github.com>
2022-12-19 22:46:23 +0200

    token bearer

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import 'dotenv/config'
     PassportModule,     
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '3600s' },
+      signOptions: { expiresIn: '360d' },
     }),
   ],
   providers: [
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Post, Get, Put, Delete, Body, Param, Query, NotFoundException, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Body, Param, Query, NotFoundException, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { FilterProductDTO } from './dtos/filter-product.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enums/role.enum';
 
 @Controller('store/products')
 export class ProductController {
@@ -31,20 +35,25 @@ export class ProductController {
     if (!product) throw new NotFoundException('Product does not exist!');
     return product;
   }
-
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User)
   @Post('/')
   async addProduct(@Body() createProductDTO: CreateProductDTO) {
     const product = await this.productService.addProduct(createProductDTO);
     return product;
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User)
   @Put('/:id')
   async updateProduct(@Param('id') id: string, @Body() createProductDTO: CreateProductDTO) {
     const product = await this.productService.updateProduct(id, createProductDTO);
     if (!product) throw new NotFoundException('Product does not exist!');
     return product;
   }
-
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User)
   @Delete('/:id')
   async deleteProduct(@Param('id') id: string) {
     const product = await this.productService.deleteProduct(id);
